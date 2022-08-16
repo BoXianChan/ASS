230553593d7de1fa09ab9d86076b085890378548
BoXianChan <111367732+BoXianChan@users.noreply.github.com>
2022-08-16 22:24:07 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<!--
+Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+Click nbfs://nbhost/SystemFileSystem/Templates/Project/PHP/PHPProject.php to edit this template
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>BuiBui Basketball Society</title>
+    </head>
+    <body>
+        <h1>Welcome to BuiBui Basketball Society !!</h1>
+        <?php
+        
+        ?>
+    </body>
+</html>
